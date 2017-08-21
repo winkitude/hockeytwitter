735564d9ccb09818982f4afb36198b685b8c7aee
Ari Winkleman <ariswinkleman@gmail.com>
2017-08-21 11:00:05 -0400

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -9,9 +9,9 @@ MY_ACCESS_TOKEN_KEY = '	899617065375608832-aLNvrdAgFitNyh7S1elnsINQJqWoJQ4'
 MY_ACCESS_TOKEN_SECRET = 'nT4R2m2cE0ApjxDu83gEObeaYySelIjp0OrNfT6S9jW99'
 
 SOURCE_ACCOUNTS = ["TheHockeyNews"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 8 #How often do you want this to run? 1/8 times?
+ODDS = 4 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = True #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
-TWEET_ACCOUNT = "" #The name of the account you're tweeting to.
+TWEET_ACCOUNT = "HockeyNewsBot" #The name of the account you're tweeting to.
