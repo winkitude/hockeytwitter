970d0fb855cd9509b57d384908ca7643ce5528a2
Ari Winkleman <ariswinkleman@gmail.com>
2017-08-21 10:54:09 -0400

    Update local_settings_example.py

--- local_settings_example.py
+++ local_settings_example.py
@@ -3,12 +3,12 @@ Local Settings for a heroku_ebooks account. #fill in the name of the account you
 '''
 
 #configuration
-MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
-MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
-MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
-MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+MY_CONSUMER_KEY = 'gFrsipzfT4vtFWt63xqidpAxA'
+MY_CONSUMER_SECRET = 'gLeLQ53ykObJqWAHQnbisizd8GPlzr5cwW9II3yM7IZjCo524j'
+MY_ACCESS_TOKEN_KEY = '	899617065375608832-aLNvrdAgFitNyh7S1elnsINQJqWoJQ4'
+MY_ACCESS_TOKEN_SECRET = 'nT4R2m2cE0ApjxDu83gEObeaYySelIjp0OrNfT6S9jW99'
 
-SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+SOURCE_ACCOUNTS = ["TheHockeyNews"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 8 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = True #Set this to False to start Tweeting live
